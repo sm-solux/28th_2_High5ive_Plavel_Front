12a02d312a0e4ee00fd6801329975050a43fd3f4
solux <lee.yeji0327@gmail.com>
2024-01-06 17:06:49 +0900

    add landingpage, signuppage

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            랜딩페이지 및 첫페이지
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignupPage = () => {
+    return (
+        <div>
+            회원가입페이지
+        </div>
+    );
+};
+
+export default SignupPage;

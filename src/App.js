05b3671b33204651f580c7d13d6b754d54955a3a
solux <lee.yeji0327@gmail.com>
2024-01-06 17:08:47 +0900

    add app.js routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import LandingPage from './pages/LandingPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+      </Routes>
     </>
   );
 }

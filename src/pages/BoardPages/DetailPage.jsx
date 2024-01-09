d58d2239c12d39a16ac2ba8117898de7afade2c4
solux <lee.yeji0327@gmail.com>
2024-01-09 13:52:43 +0900

    [add] detailpage 생성

---
+++ src/pages/BoardPages/DetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailPage = () => {
+    return (
+        <div>
+            세부페이지
+        </div>
+    );
+};
+
+export default DetailPage;
---
+++ src/pages/BoardPages/HotPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotPage = () => {
+    return (
+        <div>
+            인기페이지
+        </div>
+    );
+};
+
+export default HotPage;

124942a4f44b8cb83495d2ab8ed0104d52de4d5e
solux <lee.yeji0327@gmail.com>
2024-01-09 13:49:08 +0900

    [add] hompage에 topbar 추가

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 10vh;
+    background-color: aliceblue;
+    position: fixed;
+    top: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 100;
+`
 
 const TopBar = () => {
     return (
-        <div>
+        <Wrapper>
             
-        </div>
+        </Wrapper>
     );
 };
 
--- src/pages/BoardPages/HomePage.jsx
+++ src/pages/BoardPages/HomePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import TopBar from '../../components/TopBar';
+
+const Body = styled.div`
+    margin-top: 10vh;
+`
 
 const HomePage = () => {
     return (
-        <div>
-            게시판 홈
-        </div>
+        <>
+            <TopBar/>
+            <Body>
+                게시판 홈
+            </Body>
+        </>
     );
 };
 

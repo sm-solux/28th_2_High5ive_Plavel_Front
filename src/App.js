b83081e354efa3c0c2ae787d880355e7baf7f15a
solux <lee.yeji0327@gmail.com>
2024-01-09 13:55:37 +0900

    [add] app.js 라우팅

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import LandingPage from './pages/LandingPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/BoardPages/HomePage';
+import RecentPage from './pages/BoardPages/RecentPage';
+import HotPage from './pages/BoardPages/HotPage';
+import DetailPage from './pages/BoardPages/DetailPage';
+import WritePage from './pages/BoardPages/WritePage';
 
 function App() {
   return (
@@ -10,7 +14,13 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
+
         <Route path='/home' element={<HomePage/>}/>
+        <Route path='/recent' element={<RecentPage/>}/>
+        <Route path='/hot' element={<HotPage/>}/>
+        <Route path='/detail' element={<DetailPage/>}/>
+        <Route path='/write' element={<WritePage/>}/>
+        
       </Routes>
     </>
   );

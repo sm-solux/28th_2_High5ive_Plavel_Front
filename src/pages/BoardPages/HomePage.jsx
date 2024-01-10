6482c384f34167c34f49f7ae0682a57b23e720c9
solux <lee.yeji0327@gmail.com>
2024-01-10 18:28:57 +0900

    [feat] banner 구현

--- src/pages/BoardPages/HomePage.jsx
+++ src/pages/BoardPages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TopBar from '../../components/TopBar';
+import Banner from '../../components/Banner';
 
 const Body = styled.div`
     margin-top: 10vh;
@@ -11,6 +12,7 @@ const HomePage = () => {
         <>
             <TopBar/>
             <Body>
+                <Banner/>
                 게시판 홈
             </Body>
         </>

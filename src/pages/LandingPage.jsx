3e4f52c429646a5f25b955faa196aa5d7a772ab4
C2hazelnut <c2yjcs03@sookmyung.ac.kr>
2024-01-19 20:24:31 +0900

    랜딩페이지 버튼 제거

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,9 +8,6 @@ const LandingPage = () => {
     return (
         <div>
             랜딩페이지 및 첫페이지
-            <div>
-            <button onClick={handleButtonClick}>테스트 시작</button>
-            </div>
         </div>
     );
 };

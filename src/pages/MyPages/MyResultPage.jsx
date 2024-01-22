cbd44dc9c19c347c4b1a39144fc09bc5f7e4da35
solux <lee.yeji0327@gmail.com>
2024-01-22 13:05:15 +0900

    [feat] mybar 구현

--- src/pages/MyPages/MyResultPage.jsx
+++ src/pages/MyPages/MyResultPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import TopBar from '../../components/TopBar';
+import MyBar from '../../components/MyBar';
+
+const Body = styled.div`
+    margin-top: 10vh;
+`
 
 const MyResultPage = () => {
     return (
-        <div>
-            나의 테스트 결과
-        </div>
+        <>
+        <TopBar/>
+        <Body>
+            <MyBar/>
+        </Body>
+        </>
     );
 };
 
